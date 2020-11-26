56b17453213e82d4af7afd24a4a6abb22df87b4c
Mehul Mohan <mehulmpt@gmail.com>
2020-11-26 05:46:08 +0530

    default files added

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-function rot13(str) {
-	return str
+function checkCashRegister(price, cash, cid) {
+	var change
+	return change
 }
 
-rot13('SERR PBQR PNZC')
+checkCashRegister(19.5, 20, [
+	['PENNY', 1.01],
+	['NICKEL', 2.05],
+	['DIME', 3.1],
+	['QUARTER', 4.25],
+	['ONE', 90],
+	['FIVE', 55],
+	['TEN', 20],
+	['TWENTY', 60],
+	['ONE HUNDRED', 100]
+])
 
 // do not edit below this line
-module.exports = rot13
+module.exports = checkCashRegister
