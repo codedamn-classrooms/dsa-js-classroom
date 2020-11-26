e8c24108802f502f2e6a64516ed266ed0c948c8e
Mehul Mohan <mehulmpt@gmail.com>
2020-11-26 05:46:27 +0530

    default files added

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-function checkCashRegister(price, cash, cid) {
-	var change
-	return change
+function telephoneCheck(str) {
+	return true
 }
 
-checkCashRegister(19.5, 20, [
-	['PENNY', 1.01],
-	['NICKEL', 2.05],
-	['DIME', 3.1],
-	['QUARTER', 4.25],
-	['ONE', 90],
-	['FIVE', 55],
-	['TEN', 20],
-	['TWENTY', 60],
-	['ONE HUNDRED', 100]
-])
+telephoneCheck('555-555-5555')
 
 // do not edit below this line
-module.exports = checkCashRegister
+module.exports = telephoneCheck
